ba0b24da4a9e9e8724012b1926e4897134e6ccb5
Rick South <rick@natamus.com>
2023-12-12 10:50:52 +0100

    Biome Spawn Point source code for 1.19.4.

---
+++ Forge/src/main/java/com/natamus/biomespawnpoint/forge/events/ForgeBiomeSpawnEvent.java
@@ -0,0 +1,29 @@
+package com.natamus.biomespawnpoint.forge.events;
+
+import com.mojang.logging.LogUtils;
+import com.natamus.biomespawnpoint.events.BiomeSpawnEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ServerLevelData;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import org.slf4j.Logger;
+
+@EventBusSubscriber
+public class ForgeBiomeSpawnEvent {
+	private static final Logger logger = LogUtils.getLogger();
+
+	@SubscribeEvent(receiveCanceled = true)
+	public void onWorldLoad(LevelEvent.CreateSpawnPosition e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (BiomeSpawnEvent.onWorldLoad((ServerLevel)level, (ServerLevelData)level.getLevelData())) {
+			e.setCanceled(true);
+		}
+	}
+}
