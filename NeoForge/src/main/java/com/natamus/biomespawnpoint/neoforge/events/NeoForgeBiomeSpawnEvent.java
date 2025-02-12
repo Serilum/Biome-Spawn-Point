a6b95de57eb2a126bea5606fbe4f5b46eee0e5a2
Rick South <rick@natamus.com>
2025-02-12 23:43:19 +0100

    (v2.4) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/biomespawnpoint/neoforge/events/NeoForgeBiomeSpawnEvent.java
+++ NeoForge/src/main/java/com/natamus/biomespawnpoint/neoforge/events/NeoForgeBiomeSpawnEvent.java
@@ -8,10 +8,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.ServerLevelData;
 import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import org.slf4j.Logger;
 
-@EventBusSubscriber
 public class NeoForgeBiomeSpawnEvent {
 	private static final Logger logger = LogUtils.getLogger();
 
