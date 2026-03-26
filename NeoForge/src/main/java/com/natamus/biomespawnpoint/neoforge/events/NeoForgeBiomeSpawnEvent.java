7a144b08047aafab9158f436528283544e2a9524
Rick South <rick@natamus.com>
2026-03-26 02:03:12 +0100

    Biome Spawn Point source code for 26.1.0.

---
+++ NeoForge/src/main/java/com/natamus/biomespawnpoint/neoforge/events/NeoForgeBiomeSpawnEvent.java
@@ -0,0 +1,27 @@
+package com.natamus.biomespawnpoint.neoforge.events;
+
+import com.mojang.logging.LogUtils;
+import com.natamus.biomespawnpoint.events.BiomeSpawnEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ServerLevelData;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import org.slf4j.Logger;
+
+public class NeoForgeBiomeSpawnEvent {
+	private static final Logger logger = LogUtils.getLogger();
+
+	@SubscribeEvent(receiveCanceled = true)
+	public static void onWorldLoad(LevelEvent.CreateSpawnPosition e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (BiomeSpawnEvent.onWorldLoad((ServerLevel)level, (ServerLevelData)level.getLevelData())) {
+			e.setCanceled(true);
+		}
+	}
+}
