3f2dc227c6a319d6d1dafdb6f461c729bd46d926
Rick South <rick@natamus.com>
2026-04-02 16:19:57 +0200

    Biome Spawn Point source code for 26.1.1.

---
+++ Forge/src/main/java/com/natamus/biomespawnpoint/ModForge.java
@@ -0,0 +1,37 @@
+package com.natamus.biomespawnpoint;
+
+import com.natamus.biomespawnpoint.forge.events.ForgeBiomeSpawnEvent;
+import com.natamus.biomespawnpoint.util.Reference;
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	ForgeBiomeSpawnEvent.registerEventsInBus();
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
