0102c9349e09ced253feffb8727aaa28fcee61a3
Rick South <rick@natamus.com>
2026-04-10 03:01:45 +0200

    Biome Spawn Point source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/biomespawnpoint/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.biomespawnpoint.util;
+
+public class Reference {
+	public static final String MOD_ID = "biomespawnpoint";
+	public static final String NAME = "Biome Spawn Point";
+	public static final String VERSION = "2.5";
+	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+}
